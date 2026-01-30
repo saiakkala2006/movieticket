9a61532c2e274e1e24d6c78a6ad28e734cf105d7
Sai Akkala <saiakkala2006@gmail.com>
2026-01-30 19:15:16 +0530

    30/1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import AdminHome from "./components/AdminHome";
+import MovieDetails from "./components/MovieDetails";
+import BookingConfirm from "./components/BookingConfirm";
+import SeatSelection from "./components/SeatSelection";
+
+
+
 import "./App.css";
 
 function App() {
@@ -11,6 +17,12 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/admin" element={<AdminHome />} />
+        <Route path="/movie/:name" element={<MovieDetails />} />
+        <Route path="/confirm" element={<BookingConfirm />} />
+        <Route path="/seats" element={<SeatSelection />} />
+
+
+
       </Routes>
     </BrowserRouter>
   );
